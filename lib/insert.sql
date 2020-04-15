7605117ce80a7f5a8d3a0cab74b36160ba56033c
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-15 06:26:42 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (id, name, age, gender, color, temperament, alive)
-INSERT INTO bears
-INSERT INTO bears
-INSERT INTO bears
-INSERT INTO bears
-INSERT INTO bears
-INSERT INTO bears
-INSERT INTO bears
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES 
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES 
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES 
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES 
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES 
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
